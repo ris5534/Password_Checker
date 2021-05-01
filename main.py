32cea7d3d4198683acc6e83558a84668adb1107b
gericgonz <gonzalez7eric@gmial.com>
2021-05-01 17:04:18 -0400

    Checks everything

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 # Start of project
 password = str(input())
-#contains_digit = False
+special_characters = ["!", "@", "#", "$", "^", "&", "*", "(", ")", "_"]
 
 if len(password) <= 6:
-    print("Password not long enough")
+    print("ERROR: Password not long enough")
+else:
+    print("PASS: There is more than 6 characters")
 
 for character in password:
     if character.isdigit():
-        print("There is a number in the password")
+        print("PASS: There is a digit")
         break
 else:
-    print("No Digit in password")
+    print("ERROR: No Digit")
+for character in special_characters:
+    if character in password:
+        print("PASS: There is a special character")
+        break
+else:
+    print("ERROR: No Special Character")
+# Checking for Uppercase letters
+for character in password:
+    if character.isupper():
+        print("PASS: There is an uppercase letter")
+        break
+else:
+    print("ERROR: NO uppercase letter")
+# Checking for lowercase letter
+for character in password:
+    if character.islower():
+        print("PASS: There is lowercase letter")
+        break
+else:
+    print("ERROR: No lowercase letter")
